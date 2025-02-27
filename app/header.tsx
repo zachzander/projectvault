f5616dd238434b3916745f9e3f6d2e1826483023
Zainab Zaiba A <97389036+xynabzander@users.noreply.github.com>
2025-02-27 16:50:21 +0530

    third commit

--- app/header.tsx
+++ app/header.tsx
@@ -1,10 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationProfile,UserButton } from "@clerk/nextjs";
 export function Header() {
     return ( 
     <div className="border-b py-4 bg-gray-50">
          <div className="items-center container mx-auto justify-between flex">
-            ProjectVault
-            <UserButton />
+                <div>ProjectVault</div>
+                <div className="flex gap-2">
+                <OrganizationProfile />
+                <UserButton />
+                </div>              
          </div>
     </div>
     );
